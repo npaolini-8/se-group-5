a0e5724ad02d8d22cca513daf11b08715b59c57a
Nick Paolini <npaolini@gmail.com>
2021-11-28 01:50:59 -0500

    corrected warehouse sub item

--- warehouse.py
+++ warehouse.py
@@ -88,7 +88,7 @@ class Warehouse():
             }
         )
 
-    def create_sub_item(self, Name, container):
+    def create_sub_item(self, user, Name, container=None):
         barcode = self.generate_barcode(Name, self.get_item_increment(Name)+1)
         self.items_collection.update_one(
             {"Name" : Name},
@@ -99,7 +99,7 @@ class Warehouse():
                     "Container":container,
                     "Status":"Available",
                     "Date modified": self.get_time(),
-                    "Last modified by":"get_user()"
+                    "Last modified by":user
                     }
                 }
             }
