787606e4f842812a0ffe571311792a16436b021b
johnst35 <johnst35@students.rowan.edu>
2021-12-06 01:19:07 -0500

    Update Security.py

--- Security.py
+++ Security.py
@@ -1,20 +1,28 @@
 import os
 import hashlib
 
-def security(username, password):
-
-    users = {}
+class password():
     
-    salt = os.urandom(32)
-    key = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), salt, 100000)
-    users[username] = {'salt' : salt, 'key' : key}
+    # Generates a password given a username and password
+    def generate_password(username, password):
 
-    # Checking to see if the orignal key is the same as the old key
-    salt = users[username]['salt']
-    key = users[username]['key']
-    new_key = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), salt, 100000)
-
-    assert key == new_key
-
+        users = {}
+        salt = os.urandom(32)
+        plaintext = 'password'.encode()
     
+        key = hashlib.pbkdf2_hmac('sha512', plaintext, salt, 100000)
+        hex_hash = key.hex()
+        users[username] = {'salt' : salt, 'key' : key}
 
+    # Checks to see if a the new hash of a password is the same as the original
+    def check_password(self, username, password, salt):
+
+        # Checking to see if the orignal key is the same as the old key
+        salt = self.users[username]['salt']
+        key = self.users[username]['key']
+        new_hex = hashlib.pbkdf2_hmac('sha512', self.plaintext, salt, 100000)
+
+        if self.hex_hash == new_hex:
+            return True
+        else:
+            return False
