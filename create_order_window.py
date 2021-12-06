5562ce6b3a97e8915953df5fce4ad29887d68e67
frogkm <53508449+frogkm@users.noreply.github.com>
2021-12-06 17:54:57 -0500

    Made table headers work

--- create_order_window.py
+++ create_order_window.py
@@ -262,7 +262,7 @@ class CreateOrderWindow(QDialog):
             item_name = self.ui.order_items_tbl.item(row, 0).text()
             item_count = int(self.ui.order_items_tbl.item(row,  1).text())
             self.warehouse_controller.warehouse.add_to_order(order_id, item_name, item_count, order_type)
-        
+
         self.ui.order_items_tbl.setRowCount(0)
         self.ui.order_items_tbl.insertRow(self.ui.order_items_tbl.rowCount())
         self.ui.client_line.setText('')
@@ -295,15 +295,25 @@ class CreateOrderWindow(QDialog):
 
     def refresh_table(self):
         self.init_table(self.ui.search_tbl, self.warehouse_controller.get_items())
-    
+
     def init_tables(self):
         self.ui.search_tbl.setSelectionBehavior(QAbstractItemView.SelectionBehavior.SelectRows)
         self.ui.search_tbl.setSelectionMode(QAbstractItemView.SingleSelection)
         self.ui.order_items_tbl.setSelectionBehavior(QAbstractItemView.SelectionBehavior.SelectRows)
         self.ui.order_items_tbl.setSelectionMode(QAbstractItemView.SingleSelection)
+
+
         items=[{'Item Name': '', 'Stock': ''}]
         self.init_table(self.ui.search_tbl, self.warehouse_controller.get_items())
         self.init_table(self.ui.order_items_tbl, items)
+
+        header = self.ui.search_tbl.horizontalHeader()
+        header.setSectionResizeMode(0, QHeaderView.Stretch)
+        header.setSectionResizeMode(1, QHeaderView.Stretch)
+
+        header = self.ui.order_items_tbl.horizontalHeader()
+        header.setSectionResizeMode(0, QHeaderView.Stretch)
+        header.setSectionResizeMode(1, QHeaderView.Stretch)
 
         def clear_amount():
             self.ui.count_line.setText('0')
